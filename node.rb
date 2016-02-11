09c34d59522ba0ddf4b9d8800bc49b118713bd2f
Christopher Soden <karavol27@gmail.com>
2016-02-10 23:44:19 -0700

    removed to_s from node.rb

--- node.rb
+++ node.rb
@@ -6,12 +6,5 @@ class Node
     @value = value
     @pointer = pointer
   end
-
-  def to_s
-    array_representation = []
-    array_representation << @value
-    array_representation << @pointer
-    array_representation.inspect
-  end
-
+  
 end
